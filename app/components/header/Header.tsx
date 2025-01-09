5faccfd80cf76ceb5b8910f33eb43ee5dc2e40c0
Souda Abhinav <soudaabhinav@gmail.com>
2025-01-09 16:22:56 +0530

    first commit

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,55 @@
+import { useStore } from '@nanostores/react';
+import { ClientOnly } from 'remix-utils/client-only';
+import { chatStore } from '~/lib/stores/chat';
+import { classNames } from '~/utils/classNames';
+import { HeaderActionButtons } from './HeaderActionButtons.client';
+import { ChatDescription } from '~/lib/persistence/ChatDescription.client';
+
+export function Header() {
+  const chat = useStore(chatStore);
+
+  return (
+    <header
+      className={classNames(
+        'flex items-center bg-bolt-elements-background-depth-1 p-5 border-b h-[var(--header-height)]',
+        {
+          'border-transparent': !chat.started,
+          'border-bolt-elements-borderColor': chat.started,
+        },
+      )}
+    >
+      <div className="flex items-center gap-2 z-logo text-bolt-elements-textPrimary cursor-pointer">
+        <div className="i-ph:sidebar-simple-duotone text-xl" />
+        <div>
+      {/* Preconnect and Link to Google Fonts in the Head (can be added in public/index.html for production) */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Metal+Mania&display=swap"
+        rel="stylesheet"
+      />
+
+      {/* Link containing Metal Mania font applied to AutoLit */}
+      <a href="/" className="text-2xl font-semibold text-accent flex items-center">
+        <span className="i-bolt:logo?mask w-[46px] inline-block"></span>
+        <span style={{ fontFamily: "'Metal Mania', sans-serif" }}>AutoLit</span>
+      </a>
+    </div>
+      </div>
+      {chat.started && ( // Display ChatDescription and HeaderActionButtons only when the chat has started.
+        <>
+          <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
+            <ClientOnly>{() => <ChatDescription />}</ClientOnly>
+          </span>
+          <ClientOnly>
+            {() => (
+              <div className="mr-1">
+                <HeaderActionButtons />
+              </div>
+            )}
+          </ClientOnly>
+        </>
+      )}
+    </header>
+  );
+}
